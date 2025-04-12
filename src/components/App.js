6828e4f6cf097edb56d9b491e32accc7873748a5
debayudh2001 <debayudh31@gmail.com>
2025-04-13 01:09:25 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [weather, setWeather] = useState({ temperature: 25, conditions: "Sunny" })
+  //const [check, setCheck] = useState(false)
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <span className={weather.temperature > 20 ? "red" : "blue"}>
+        <p>Temperature: {weather.temperature}</p>
+        <p>Conditions: {weather.conditions}</p>
+      </span>
     </div>
   )
 }
